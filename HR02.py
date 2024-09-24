e5b88eddd5ae9476f3a5533d873db34f2a13b54f
M Umer Afzal <me.mumerafzal@gmail.com>
2024-09-24 19:07:13 +0500

    1.2

---
+++ HR02.py
@@ -0,0 +1,9 @@
+Item1 = 'Phone'
+Item1_price = 100
+Item1_quantity = 5
+Item1_price_total = Item1_price * Item1_quantity
+
+print(type(Item1))
+print(type(Item1_price))
+print(type(Item1_quantity))
+print(type(Item1_price_total))
